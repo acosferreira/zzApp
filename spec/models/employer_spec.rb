fdf9df731d81b374826871b7d5f86cffdb1f05e9
Ana Carolina Ferreira <acosferreira@gmail.com>
2021-03-18 23:55:59 -0700

    change spec context

--- spec/models/employer_spec.rb
+++ spec/models/employer_spec.rb
@@ -7,31 +7,24 @@ RSpec.describe Employer, type: :model do
 	let(:employer) {create(:employer, :AcmeCo)}
 	let(:employee) {create(:employee, :ana, employer: employer)}
 	let(:employeer_csv) { create_list(:employeer_csv, 3, :external_ref_acme, employer: employer )}
-	let(:file){"spec/fixtures/AcmeCo.csv"}
 	  	
   	context 'when type is external_ref' do
+  		let(:file){"spec/fixtures/AcmeCo.csv"}
+	
   		it 'employer have earnings' do
 			 expect(employee.name).to eq 'test'
 			 expect(employeer_csv).to_not eq ''
 			 expect(subject.ids.size).to eq 1			
 		end
   	end
-  end  
 
-  context '#load_earnings AcmeCo Full' do
-	subject {employer.load_earnings(file)}
-	let(:employer) {create(:employer, :AcmeCo)}
-	let(:employee) {create(:employee, :ana, employer: employer)}
-	let(:employeer_csv) { create_list(:employeer_csv, 3, :external_ref_acme, employer: employer )}
-	let(:file){"spec/fixtures/AcmeCoFull.csv"}
-	  	
   	context 'when type is external_ref' do
+  		let(:file){"spec/fixtures/AcmeCoFull.csv"}
+	
   		it 'employer have earnings' do
 			 expect(employee.name).to eq 'test'
 			 expect(employeer_csv).to_not eq ''
 			 expect(subject.ids.size).to eq 100
-			 # expect(subject.count).to eq 100	
-
 		end
   	end
   end  
